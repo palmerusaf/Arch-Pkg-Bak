20e60b425ddda96cf1deefb1f737d1c0c00d8dbb
Branden Palmer <brandenpalmer08@gmail.com>
2025-03-11 12:00:40 +0700

    add relative paths

--- backup-pkgs.sh
+++ backup-pkgs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # offical packages
-pacman -Qqen > pkglist-repo.txt
+pacman -Qqen > ./pkglist-repo.txt
 # aur packages 
-pacman -Qqem > pkglist-aur.txt
+pacman -Qqem > ./pkglist-aur.txt
--- restore-pkgs.sh
+++ restore-pkgs.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # offical
-sudo pacman -S --needed - < pkglist-repo.txt
+sudo pacman -S --needed - < ./pkglist-repo.txt
 # aur
-yay -S --needed - < pkglist-aur.txt
+yay -S --needed - < ./pkglist-aur.txt
