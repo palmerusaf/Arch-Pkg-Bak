354dfac9c10bff79c601a46c797b9d878939843c
Branden Palmer <brandenpalmer08@gmail.com>
2025-03-11 11:46:27 +0700

    init

---
+++ backup-pkgs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# offical packages
+pacman -Qqen > pkglist-repo.txt
+# aur packages 
+pacman -Qqem > pkglist-aur.txt
---
+++ restore-pkgs.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# offical
+sudo pacman -S --needed - < pkglist-repo.txt
+# aur
+yay -S --needed - < pkglist-aur.txt
